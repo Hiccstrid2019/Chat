477280918e8726dd3f8a81c551c2ffeb938017d0
Hiccstrid2019 <hiccup.colin@yandex.ru>
2021-08-04 17:22:03 +0300

    Added Repository pattern

---
+++ ChatServer/DAL/EFUserRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ChatServer.Models;
+
+namespace ChatServer.DAL
+{
+    public class EFUserRepository : IRepository
+    {
+        private UsersContext context = new UsersContext();
+
+        public void Create(ApplicationUser user)
+        {
+            context.ApplicationUsers.Add(user);
+            SaveChanges();
+        }
+        public ApplicationUser Get(string username)
+        {
+            return context.ApplicationUsers.FirstOrDefault(user => user.UserName == username);
+        }
+
+        public List<string> GetAllUserNames()
+        {
+            return (from u in context.ApplicationUsers
+                    orderby u.UserName
+                    select u.UserName).ToList();
+        }
+
+        private void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ChatServer/DAL/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using ChatServer.Models;
+using System.Collections.Generic;
+
+namespace ChatServer.DAL
+{
+    public interface IRepository
+    {
+        void Create(ApplicationUser user);
+        ApplicationUser Get(string username);
+        List<string> GetAllUserNames();
+    }
+}
