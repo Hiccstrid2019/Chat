ad570cbd79ecc1a1cd48c2923a88d3858e1f608b
Hiccstrid2019 <hiccup.colin@yandex.ru>
2021-08-04 13:36:53 +0300

    Added ChatService interface

---
+++ ChatClient/Services/IChatService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ChatClient.Services
+{
+    public interface IChatService
+    {
+        void Connect();
+        void SendTextMessage(string name, string sender, string message);
+        void LogIn(string name, string password);
+        void GetAllUsers();
+        void CreateGroup(string groupName, IList<string> usernames);
+    }
+}
