6332ffcfbf717a6121d5b7de1c77ac8b9356df41
Gleb <schugin.gleb@yandex.ru>
2022-11-08 16:30:27 +0300

    change hub url

--- ChatClient/Services/ChatService.cs
+++ ChatClient/Services/ChatService.cs
@@ -7,22 +7,22 @@ namespace ChatClient.Services
 {
     class ChatService : IChatService
     {
-        private HubConnection connection;
-        private string Url = "https://localhost:44397/chatHub";
+        private HubConnection _connection;
+        private string Url = "https://localhost:5001/chatHub";
         public event Action<string, string> NewTextMessageRecived;
         public event Action<string, string, string> NewTextMessageFromGroup;
         public event Action Logged;
         public event Action<List<string>> ShowAllUsers;
         public void Connect()
         {
-            connection = new HubConnectionBuilder().WithUrl(Url).Build();
+            _connection = new HubConnectionBuilder().WithUrl(Url).Build();
             
-            connection.On("Logged", () => Logged());
-            connection.On<List<string>>("ShowAllUsers", (allUsers) => ShowAllUsers(allUsers));
-            connection.On<string, string>("addNewMessageToPage", (name, message) => NewTextMessageRecived(name, message));
-            connection.On<string, string, string>("AddNewMessageFromGroup", (groupname, sender, message) => NewTextMessageFromGroup(groupname, sender, message));
+            _connection.On("Logged", () => Logged());
+            _connection.On<List<string>>("ShowAllUsers", (allUsers) => ShowAllUsers(allUsers));
+            _connection.On<string, string>("addNewMessageToPage", (name, message) => NewTextMessageRecived(name, message));
+            _connection.On<string, string, string>("AddNewMessageFromGroup", (groupname, sender, message) => NewTextMessageFromGroup(groupname, sender, message));
             
-            connection.StartAsync().ContinueWith(task => {
+            _connection.StartAsync().ContinueWith(task => {
                 if (task.IsFaulted)
                 {
                     MessageBox.Show("Сервер врменно недоступен", "Сообщение");
@@ -31,21 +31,21 @@ namespace ChatClient.Services
         }
         public async void SendTextMessage(string name, string sender, string message)
         {
-            await connection.InvokeAsync("Send", name, sender, message);
+            await _connection.InvokeAsync("Send", name, sender, message);
         }
         public async void LogIn(string name, string password)
         {
-            await connection.InvokeAsync("LogIn", name, password);
+            await _connection.InvokeAsync("LogIn", name, password);
         }
 
         public async void GetAllUsers()
         {
-            await connection.InvokeAsync("GetAllUsers");
+            await _connection.InvokeAsync("GetAllUsers");
         }
 
         public async void CreateGroup(string groupName, IList<string> usernames)
         {
-            await connection.InvokeAsync("CreateGroup", groupName, usernames);
+            await _connection.InvokeAsync("CreateGroup", groupName, usernames);
         }
     }
 }
